8960b1ec11a1d4d8413e52a268e20fe0c283f888
JangMotti <totok682@naver.com>
2020-03-02 13:18:25 +0900

    - Change Target Framework to 4.7.2

---
+++ Study_Virtual_Static.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    internal class Employee
+    {
+        static public Employee _inst;
+        public Int32 GetYearsEmployed() { return 0; }
+        public virtual String GetProgressReport() { return null; }
+        public static string Hello(String name) { return "Hello " + name; }
+    }
+
+    internal class Manager : Employee
+    {
+        public static new string Hello(String name) { return "HELLO " + name; }
+    }
+
+
+    class Study_Virtual_Static
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            Manager m = new Manager();
+
+            Console.WriteLine(Employee.Hello("kai"));
+            Console.WriteLine(Manager.Hello("kai"));
+        }
+    }
+}

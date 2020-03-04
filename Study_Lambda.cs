c78b766c7d89ff9ad6f56afdffb225c551a0eed6
JangMotti <totok682@naver.com>
2020-03-04 12:15:07 +0900

    - upload Lambda/Property study code

---
+++ Study_Lambda.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Threading;
+
+//namespace CSharpStudy
+//{
+//    public static class Extension
+//    {
+//        public static void InvokeAndCatch<TException>(this Action<Object> d, Object o) where TException : Exception
+//        {
+//            try { d(o); }
+//            catch (TException) { }
+//        }
+//    }
+//    class Study_Lambda
+//    {
+//        private static void Main(string[] args)
+//        {
+//            study_extension_method();
+//        }
+
+//        private static void study_lambda_function()
+//        {
+//            Thread t = new Thread(obj => { Console.WriteLine("ThreadFunc in anonymous method called!"); }) ;
+//            t.Start();
+//            t.Join();
+//        }
+
+//        private static void study_extension_method()
+//        {
+//            Action<Object> action = o => Console.WriteLine(o.GetType());
+//            action.InvokeAndCatch<NullReferenceException>(null); // 예외 발생 x
+//            action(null); // 예외 발생
+            
+//        }
+//    }
+//}
---
+++ Study_Property.cs
@@ -0,0 +1,69 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpStudy
+//{
+//    public sealed class People
+//    {
+//        public int age;
+//        public string name;
+
+//        public Object this[int key]
+//        {
+//            get
+//            {
+//                if (key == 1)
+//                    return age;
+//                else if (key == 2)
+//                    return name;
+//                return null;
+//            }
+//            set
+//            {
+//                if (key == 1)
+//                    age = (int)value;
+//                else if (key == 2)
+//                    name = (string)value;
+//            }
+//        }
+//        public Object this[string key]
+//        {
+//            get
+//            {
+//                if (key == "Age")
+//                    return age;
+//                else if (key == "Name")
+//                    return name;
+//                return null;
+//            }
+//            set
+//            {
+//                if (key == "Age")
+//                    age = (int)value;
+//                else if (key == "Name")
+//                    name = (string)value;
+//            }
+//        }
+
+//        public override string ToString()
+//        {
+//            return $"{name} : {age}";
+//        }
+//    }
+
+//    class Study_Property
+//    {
+//        private static void Main(string[] args)
+//        {
+//            People p = new People { age = 27, name = "KAI" };
+//            Console.WriteLine(p);
+//            p["Age"] = 99;
+//            Console.WriteLine(p);
+//            Console.WriteLine(p["Name"]);
+//            Console.WriteLine(p[1]);
+//        }
+//    }
+//}

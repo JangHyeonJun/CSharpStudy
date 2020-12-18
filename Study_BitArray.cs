9a26dedc2099f96bce1c44f25866636e28cf027c
JangHyoenJun <totok682@naver.com>
2020-12-18 10:59:36 +0900

    BitArray 공부

---
+++ Study_BitArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class Study_BitArray
+    {
+        static void Main()
+        {
+            InitializeBitArray2();
+        }
+
+        static void InitializeBitArray()
+        {
+            int n = 5;
+            BitArray ba = new BitArray(new int[] { n });
+            foreach (var a in ba)
+            {
+                Console.Write($"{a} ");
+                Console.Write($"{a} ");
+            }
+        }
+
+        static void InitializeBitArray2()
+        {
+
+            int n = 3;
+            BitArray ba = new BitArray(new byte[] { (byte)n });
+
+            Console.WriteLine("ba.Length = default");
+            foreach (var a in ba)
+            {
+                Console.Write($"{a} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ba.Length = 4");
+
+            ba.Length = 4;
+            foreach (var a in ba)
+            {
+                Console.Write($"{a} ");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine($"ba[0] = {ba[0]}, ba[1] = {ba[1]}, ba[2] = {ba[2]}");
+        }
+    }
+}
--- Study_Bitvector32.cs
+++ Study_Bitvector32.cs
@@ -1,62 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.Specialized;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpStudy
-{
-    class Study_Bitvector32
-    {
-        static void Main()
-        {
-            Console.WriteLine("WrongCase 1");
-            WrongCase1();
-            Console.WriteLine();
-            Console.WriteLine("WrongCase 2");
-            WrongCase2();
-        }
+//namespace CSharpStudy
+//{
+//    class Study_Bitvector32
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("WrongCase 1");
+//            WrongCase1();
+//            Console.WriteLine();
+//            Console.WriteLine("WrongCase 2");
+//            WrongCase2();
+//        }
 
-        static void WrongCase1()
-        {
-            BitVector32 bv = new BitVector32(0);
+//        static void WrongCase1()
+//        {
+//            BitVector32 bv = new BitVector32(0);
 
-            var section1 = BitVector32.CreateSection(10);
-            var section2 = BitVector32.CreateSection(10, section1);
-            var section3 = BitVector32.CreateSection(10, section2);
-            var section4 = BitVector32.CreateSection(10, section2);
+//            var section1 = BitVector32.CreateSection(10);
+//            var section2 = BitVector32.CreateSection(10, section1);
+//            var section3 = BitVector32.CreateSection(10, section2);
+//            var section4 = BitVector32.CreateSection(10, section2);
 
-            bv[section1] = 1;
-            bv[section2] = 2;
-            bv[section3] = 3;
-            bv[section4] = 4;
+//            bv[section1] = 1;
+//            bv[section2] = 2;
+//            bv[section3] = 3;
+//            bv[section4] = 4;
 
-            Console.WriteLine(bv[section1]);
-            Console.WriteLine(bv[section2]);
-            Console.WriteLine(bv[section3]);
-            Console.WriteLine(bv[section4]);
-        }
+//            Console.WriteLine(bv[section1]);
+//            Console.WriteLine(bv[section2]);
+//            Console.WriteLine(bv[section3]);
+//            Console.WriteLine(bv[section4]);
+//        }
 
-        static void WrongCase2()
-        {
-            BitVector32 bv = new BitVector32(0);
+//        static void WrongCase2()
+//        {
+//            BitVector32 bv = new BitVector32(0);
 
-            var section1 = BitVector32.CreateSection(1);
-            var section2 = BitVector32.CreateSection(2, section1);
-            var section3 = BitVector32.CreateSection(3, section2);
-            var section4 = BitVector32.CreateSection(4, section3);
+//            var section1 = BitVector32.CreateSection(1);
+//            var section2 = BitVector32.CreateSection(2, section1);
+//            var section3 = BitVector32.CreateSection(3, section2);
+//            var section4 = BitVector32.CreateSection(4, section3);
 
-            bv[section1] = 2;
-            bv[section2] = 3;
-            bv[section3] = 4;
-            bv[section4] = 5;
+//            bv[section1] = 2;
+//            bv[section2] = 3;
+//            bv[section3] = 4;
+//            bv[section4] = 5;
 
-            Console.WriteLine(bv[section1]);
-            Console.WriteLine(bv[section2]);
-            Console.WriteLine(bv[section3]);
-            Console.WriteLine(bv[section4]);
-        }
-    }
-}
+//            Console.WriteLine(bv[section1]);
+//            Console.WriteLine(bv[section2]);
+//            Console.WriteLine(bv[section3]);
+//            Console.WriteLine(bv[section4]);
+//        }
+//    }
+//}

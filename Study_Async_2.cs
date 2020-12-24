320b0e4415506307ddab7148e77d5c82f129e7c3
JangHyoenJun <totok682@naver.com>
2020-12-24 16:59:08 +0900

    Async Await 공부 - Task는 Run할 때 비동기로 실행되며 Result를 사용하거나 Wait할 때 결과값을 가져오며 동기화 할 수 있다. - async 키워드는 await이 메서드안에 포함돼있음을 나타내며, 메서드 내에서 await을 사용하면 실행 중인 Task 를 백그라운드에서 실행한 후에 해당 메서드를 호출한 스레드로 돌아간다.

---
+++ Study_Async_2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+namespace CSharpStudy
+{
+    class Study_Async_2
+    {
+        private static void Main(string[] args)
+        {
+            TestTask4();
+        }
+
+        private static void TestTask1()
+        {
+            Console.WriteLine("Main Start");
+            var task = Task1();
+            task.Start();
+            Console.WriteLine("Waiting Task...");
+
+            Console.WriteLine($"Sum = {task.Result}");
+        }
+
+        private static void TestTask2()
+        {
+            Console.WriteLine("Main Start");
+            var task = Task2();
+            Console.WriteLine("Waiting Task...");
+
+            Console.WriteLine($"Sum = {task.Result}");
+        }
+
+        private static void TestTask3()
+        {
+            Console.WriteLine("Main Start");
+            var task = Task3();
+            Console.WriteLine("Waiting Task...");
+
+            Console.WriteLine($"Sum = {task.Result}");
+        }
+
+        private static void TestTask4()
+        {
+            Console.WriteLine("Main Start");
+            var task = Task4();
+            Console.WriteLine("Waiting Task...");
+
+            Console.WriteLine($"Sum = {task.Result}");
+        }
+
+        private static Task<int> Task1()
+        {
+            Console.WriteLine("Task Start");
+            var task = new Task<int>(() =>
+            {
+                int sum = 0;
+                for (int i=0; i<10; i++)
+                {
+                    sum += i;
+                    Thread.Sleep(100);
+                }
+                return sum;
+            });
+            Console.WriteLine("Task End");
+
+            return task;
+        }
+
+        private static async Task<int> Task2()
+        {
+            Console.WriteLine("Task Start");
+            var task = await Task.Run(() =>
+            {
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += i;
+                    Thread.Sleep(100);
+                }
+                return sum;
+            });
+            Console.WriteLine("Task End");
+
+            return task;
+        }
+
+        private static Task<int> Task3()
+        {
+            Console.WriteLine("Task Start");
+            var task = Task.Run(() =>
+            {
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += i;
+                    Thread.Sleep(100);
+                }
+                return sum;
+            });
+            Console.WriteLine("Task End");
+
+            return task;
+        }
+
+        private static Task<int> Task4()
+        {
+            Console.WriteLine("Task Start");
+            var task = Task.Run(() =>
+            {
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += i;
+                    Thread.Sleep(100);
+                }
+                return sum;
+            });
+            task.Wait();
+            Console.WriteLine("Task End");
+
+            return task;
+        }
+    }
+}

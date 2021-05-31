6c9089960175194100f0199a96397e157f70d5bf
JangMotti <totok682@naver.com>
2021-05-31 19:05:15 +0900

    비동기 스터디 - Task.Factory.StartNew 메소드는 해당 태스크를 실행하기위한 전용 스레드를 만들어 실행하길 원하거나, 스레드에게 추가적인 정보 (장기적으로 작업할 task임을 알린다던가) 를 주기를 원할 때 사용한다. - 그러나 위와같은 특수한 상황이 아닐때는 .NET 4.5부터 지원하는 Task.Run을 기본적으로 사용한다. 이는 스레드풀을 사용하여 백그라운드에서 작업을 수행한다.

---
+++ Study_Async_4.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace CSharpStudy
+//{
+//    class Study_Async_4
+//    {
+//        static void Main(string[] args)
+//        {
+//            Run1();
+//        }
+
+//        static void Run1()
+//        {
+//            var parentTask = Task.Run(() =>
+//            {
+//                var childTask = Task.Factory.StartNew(() =>
+//                {
+//                    Thread.Sleep(1000);
+//                    Console.WriteLine("child task finished.");
+//                }, TaskCreationOptions.AttachedToParent);
+
+//                Console.WriteLine("parent task finished.");
+//            });
+
+//            parentTask.Wait();
+//            Console.WriteLine("Main thread finished.");
+//        }
+//    }
+//}
